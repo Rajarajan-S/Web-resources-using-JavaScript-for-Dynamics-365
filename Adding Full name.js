3c2e0a13c14ae72694cc5859cfb593debe24f9b6
Rajarajan S <88157709+Rajarajan-S@users.noreply.github.com>
2023-05-31 16:32:45 +0530

    Update Adding Full name.js

--- Adding Full name.js
+++ Adding Full name.js
@@ -2,11 +2,12 @@ function OnSave(executionContext){
 	
     var formContext = executionContext.getFormContext();
 	
-    var firstName = formContext.getAttribute("firstname").getValue();
-    var lastName = formContext.getAttribute("lastname").getValue();
+    var firstName = formContext.getAttribute("firstname").getValue(); // Get 'first name' field value
+    var lastName = formContext.getAttribute("lastname").getValue(); // Get 'last name' field value
 	
+    // Check for null conditions
     if(firstName != null && lastName != null){
-        formContext.getAttribute("fullname").setValue(firstName+" "+lastName);
+        formContext.getAttribute("fullname").setValue(firstName+" "+lastName);  // Set 'full name' field value
     }else{
         formContext.getAttribute("fullname").setValue("")
         formContext.getControl("fullname").setVisible(false);
