8a8cb94bf6b8e0c5a03bc4062e54914ad78cad7e
Rajarajan S <88157709+Rajarajan-S@users.noreply.github.com>
2023-05-31 14:51:12 +0530

    files added

---
+++ formchange.js
@@ -0,0 +1,24 @@
+function formChange(executionContext){
+	
+	var formcontext = executionContext.getFormContext();
+	var type = formcontext.getAttribute("casetypecode").getText(); 
+	
+	NavigateForm(type,formcontext);
+}
+function NavigateForm(type,formcontext){
+	
+	var curForm = formcontext.ui.formSelector.getCurrentItem();
+	var avlForms = formcontext.ui.formSelector.items.get();
+	
+	
+	// Checking the case type ahainst the form name
+	if(curForm.getLabel().toLowerCase() != type.toLowerCase()){   
+		for(var i in avlForms){
+			var form = avlForms[i];
+			if(form.getLabel().toLowerCase() == type.toLowerCase()){
+				form.navigate();
+				return true;
+			}
+		}
+	}
+}
