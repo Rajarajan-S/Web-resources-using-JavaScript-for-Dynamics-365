fc2576c775f93486660e01066130804eab61bd96
Rajarajan S <88157709+Rajarajan-S@users.noreply.github.com>
2023-05-31 15:00:43 +0530

    added files

--- Ribbon button to display another form as a tab.js
+++ Ribbon button to display another form as a tab.js
@@ -1,5 +1,6 @@
 function OnClick(){
 	
+   	// Ribbon button to display 'contact entity main form' as a pop-up tab
 	Xrm.Navigation.navigateTo({pageType:"entityrecord", entityName:"contact", formType:2}, {target: 2, position: 1, width: {value: 50, unit:"%"}});
     
 }
